0677ff68d2b8e95338f24ba44207cc3dcdaf647f
JulienDauphin <juliendauphin.openclassrooms@gmail.com>
2023-05-05 11:14:37 +0200

    Polices google ajoutées en local + scripts déchargés

--- wp-content/themes/chicdressing/functions.php
+++ wp-content/themes/chicdressing/functions.php
@@ -8,6 +8,11 @@ function chicdressing_enqueue_styles() {
 add_filter( 'big_image_size_threshold', '__return_false' );
 
 
-
+function remove_google_fonts_stylesheet() {  
+    wp_dequeue_style( 'ashe-playfair-font' );
+    wp_dequeue_style( 'ashe-opensans-font' );
+    wp_dequeue_style( 'ashe-kalam-font' );
+}
+add_action( 'wp_enqueue_scripts', 'remove_google_fonts_stylesheet', 999 );
 
  
